438b943bba8eecbf9a0686338b8223735a26be88
Brandon_Garcia <159663607+BrandonGS22b@users.noreply.github.com>
2025-05-03 12:05:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,28 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-import os
-import uvicorn
 
-# Inicializar FastAPI
 app = FastAPI()
 
-# Configurar CORS
 app.add_middleware(
     CORSMiddleware,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-    allow_origins=["*"],  # Permitir cualquier origen
+    allow_origins=["*"],
 )
 
-# Esquema de entrada
 class ChatInput(BaseModel):
     message: str
 
-# Diccionario de preguntas y respuestas
 qa_dict = {
-    "El esmalte de uñas no seca y se levanta rápido": "¡Esto puede deberse a humedad, falta de prepado de la uña o esmalte de baja calidad. Aquí tienes una guía para resolverlo: https://youtu.be/Vrw0PDAf_RI?si=rkm9ln-A92UkD9lK",
-    # ... (más preguntas y respuestas)
+    "el esmalte de uñas no seca y se levanta rápido": "¡Esto puede deberse a humedad, falta de preparado de la uña o esmalte de baja calidad! Mira esta guía: https://youtu.be/Vrw0PDAf_RI?si=rkm9ln-A92UkD9lK",
 }
 
-# Endpoint para recibir preguntas y dar respuestas
 @app.post("/chat")
 async def chat(input: ChatInput):
-    question = input.message
-    answer = qa_dict.get(question.lower())
-    
+    question = input.message.lower()
+    answer = qa_dict.get(question)
     if not answer:
         raise HTTPException(status_code=404, detail="Pregunta no encontrada.")
-    
     return {"answer": answer}
-
-# Iniciar servidor solo si el script es ejecutado directamente
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
